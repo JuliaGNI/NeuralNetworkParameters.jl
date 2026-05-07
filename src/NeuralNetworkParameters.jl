5fb8d5772892a27f44db9ee209729e37f7b5ad8f
Michael Kraus <michael.kraus@ipp.mpg.de>
2026-05-07 13:03:14 +0200

    Barebones implementation of NetworkParameters data structure.

--- src/NeuralNetworkParameters.jl
+++ src/NeuralNetworkParameters.jl
@@ -1,5 +1,18 @@
 module NeuralNetworkParameters
 
-# Write your package code here.
+using AbstractNeuralNetworks
+
+struct NetworkParameters{Keys, ValueTypes}
+    params::NamedTuple{Keys, ValueTypes}
+end
+
+@inline Base.hasproperty(p::NetworkParameters, s::Symbol) = hasproperty(p.params, s)
+@inline Base.getproperty(p::NetworkParameters, s::Symbol) = getproperty(p.params, s)
+@inline Base.getindex(p::NetworkParameters, args...) = getindex(p.params, args...)
+
+
+
+
+
 
 end
