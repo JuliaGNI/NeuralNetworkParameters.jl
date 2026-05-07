7cfece8cafd067503febb2bb4f8b0e7768847237
Documenter.jl <documenter@juliadocs.github.io>
2026-05-07 11:06:46 +0000

    build based on 5fb8d57

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#NeuralNetworkParameters","page":"Home","title":"NeuralNetworkParameters","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NeuralNetworkParameters.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"category":"section","location":"#NeuralNetworkParameters","page":"Home","text":"Documentation for NeuralNetworkParameters.\n\n","title":"NeuralNetworkParameters"}]
 }
